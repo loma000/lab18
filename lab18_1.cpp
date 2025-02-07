c668c8df54ac8ad1b7a49705bb1415aae2315b74
loma000 <90094687+loma000@users.noreply.github.com>
2025-02-07 22:10:44 +0700

    wow

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -8,13 +8,27 @@ struct Rect
 
 double overlap(Rect R1, Rect R2)
 {
+	if (R1.x <= R2.x && R1.x + R1.w >= R2.x + R2.w && R1.y >= R2.y && R1.y - R1.h <= R2.y - R2.h)
+	{
+		return R2.w * R2.h;
+	}
+	else if (R1.x > R2.x && R1.x + R1.w < R2.x + R2.w && R1.y < R2.y && R1.y - R1.h > R2.y - R2.h)
+	{
+		return R1.w * R1.h;
+	}else{
+
 	double xleft = R1.x > R2.x ? R1.x : R2.x;
-	double ybottom = R1.y > R2.y ? R1.y : R2.y;
+	double ytop = R1.y > R2.y ? R2.y : R1.y;
 	double xright = R1.x + R1.w > R2.x + R2.w ? R2.x + R2.w : R1.x + R1.w;
-	double ytop = R1.y + R1.h > R2.y + R2.h ? R2.y + R2.h : R1.y + R1.h;
+	double ybottom= R1.y - R1.h > R2.y - R2.h ?  R1.y - R1.h : R2.y - R2.h ;
 
 	double ovrwight = xright - xleft;
 	double ovheight = ytop - ybottom;
 
-	return ovrwight <= 0 || ovheight <= 0 ? 0 : ovrwight * ovheight;
+	return ovrwight <= 0 || ovheight <= 0 ? 0 : ovrwight * ovheight;}
 }
+
+
+int main(){Rect R1 = {1,1,5,5};
+Rect R2 = {2,2,5,5};	
+cout << overlap(R1,R2);	}
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,25 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1, Rect *R2)
+{
+	if (R1->x <= R2->x && R1->x + R1->w >= R2->x + R2->w && R1->y >= R2->y && R1->y - R1->h <= R2->y - R2->h)
+	{
+		return R2->w * R2->h;
+	}
+	else if (R1->x > R2->x && R1->x + R1->w < R2->x + R2->w && R1->y < R2->y && R1->y - R1->h > R2->y - R2->h)
+	{
+		return R1->w * R1->h;
+	}else{
 
+	double xleft = R1->x > R2->x ? R1->x : R2->x;
+	double ytop = R1->y > R2->y ? R2->y : R1->y;
+	double xright = R1->x + R1->w > R2->x + R2->w ? R2->x + R2->w : R1->x + R1->w;
+	double ybottom= R1->y - R1->h > R2->y - R2->h ?  R1->y - R1->h : R2->y - R2->h ;
 
+	double ovrwight = xright - xleft;
+	double ovheight = ytop - ybottom;
 
+	return ovrwight <= 0 || ovheight <= 0 ? 0 : ovrwight * ovheight;}
 }
+
