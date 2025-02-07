3421480fe370414fd89b964f585786d19d149241
loma000 <90094687+loma000@users.noreply.github.com>
2025-02-07 16:25:15 +0700

    kk

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-struct Rect{
-	double x,y,w,h;
+#include <cmath>
+struct Rect
+{
+	double x, y, w, h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2)
+{
+	double xleft = R1.x > R2.x ? R1.x : R2.x;
+	double ybottom = R1.y > R2.y ? R1.y : R2.y;
+	double xright = R1.x + R1.w > R2.x + R2.w ? R2.x + R2.w : R1.x + R1.w;
+	double ytop = R1.y + R1.h > R2.y + R2.h ? R2.y + R2.h : R1.y + R1.h;
 
+	double ovrwight = xright - xleft;
+	double ovheight = ytop - ybottom;
 
-
+	return ovrwight <= 0 || ovheight <= 0 ? 0 : ovrwight * ovheight;
 }
